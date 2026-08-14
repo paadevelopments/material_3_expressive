c27686be72a5722fffc93c4ae8ed143c7780f66e
paadevelopments <paa.code.me@gmail.com>
2026-08-14 09:11:09 +0000

    Updated web demo build.

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3697035688" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3804280111" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
